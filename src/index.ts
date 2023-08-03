f8478d07a8518f3df22080bcffa0b7d26b9e784b
SaadFarhanIdress <saadfarhan347@gmail.com>
2023-08-03 17:08:49 +0500

    feat: correct code

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,23 @@
 import { input } from "@inquirer/prompts";
 import { toTitleCase } from "./utils/toTitleCase.js";
 
-const name = (async () => await input({
-	message: "What's your name?",
-	default: "User",
-	validate: (input: string) => {
-		if (input.trim() === "") {
-			return "Name cannot be empty. Please enter your name.";
-		}
-		return true;
-	},
-	transformer: (input: string) => {
-		return toTitleCase(input);
-	},
-}))();
+(async () => {
+	const name = await input({
+		message: "What's your name?",
+		default: "User",
+		validate: (input: string) => {
+			if (input.trim() === "") {
+				return "Name cannot be empty. Please enter your name.";
+			}
+			return true;
+		},
+		transformer: (input: string) => {
+			return toTitleCase(input);
+		},
+	});
 
-console.log(`Hello, ${name}!`)
+	console.log(`Hello, ${name}!`);
 
-process.exit();
+	process.exit();
+})();
+
